c258e5bdf82acffc27896c842f53180524d09d2c
pgb0806 <pgb.tamil.0806@gmail.com>
2025-06-17 14:08:56 +0000

    Add generate_video.py

--- generate_video.py
+++ generate_video.py
@@ -5,11 +5,13 @@ parser.add_argument("--video_url")
 parser.add_argument("--music_url")
 parser.add_argument("--text")
 parser.add_argument("--author")
+parser.add_argument("--reference")
 args = parser.parse_args()
 
 print("🎥 Video URL:", args.video_url)
 print("🎶 Music URL:", args.music_url)
 print("📝 Text:", args.text)
 print("🗿 Author:", args.author)
+print("Reference: ",args.reference)
 
 # Now use these instead of hardcoded values.
